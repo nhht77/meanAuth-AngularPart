27816fb9b989b980d390f578ee6bc53ba5bb036c
nhht77 <35263094+nhht77@users.noreply.github.com>
2018-10-14 18:50:00 +0300

    make it better

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
   authenticateUser(user) {
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('https://localhost:3000/users/authenticate', user, {headers: headers})
+    return this.http.post('users/authenticate', user, {headers: headers})
     .pipe(map(res => res.json()));
   }
 
@@ -32,7 +32,7 @@ export class AuthService {
     this.loadToken();
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type', 'application/json');
-    return this.http.get('https://localhost:3000/users/profile', {headers: headers})
+    return this.http.get('users/profile', {headers: headers})
     .pipe(map(res => res.json()));
   }
 
